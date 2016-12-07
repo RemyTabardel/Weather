dc9ba69e17c98d68a8dc7fc79db244d4e8783d22
Lyofen <remytabardel@live.fr>
2016-12-07 21:58:51 +0100

    Initial commit

---
+++ app/src/main/java/com/tabardel/weather/ui/activities/WeatherActivity.java
@@ -0,0 +1,19 @@
+package com.tabardel.weather.ui.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.tabardel.weather.R;
+
+/**
+ * Created by TABARDEL_Remy on 07/12/2016.
+ */
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+}
